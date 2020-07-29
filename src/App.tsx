46d2d743414c438c3c31ff1972b6cc8eae8db859
zoranaism <lazareviczorana@gmail.com>
2020-07-29 21:18:15 +0200

    types of props, input with refs, cross-component types, state and types, types for third party libs like router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
+// import { Route } from 'react-router-dom'; // not supported by TypeScript, so we add @types/react-router-dom
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id === todoId);
+    });
+  };
+
+  return (
+    <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
+    </div>
+  );
+};
 
 export default App;
